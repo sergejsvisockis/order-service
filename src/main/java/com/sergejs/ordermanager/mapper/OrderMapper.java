59cbf08cb307fc136dffbda61eb9302f3e7482a1
Sergejs Visockis <sergei.visotsky@gmail.com>
2024-03-16 09:41:02 +0100

    clean up + javadoc

--- src/main/java/com/sergejs/ordermanager/mapper/OrderMapper.java
+++ src/main/java/com/sergejs/ordermanager/mapper/OrderMapper.java
@@ -1,19 +1,13 @@
 package com.sergejs.ordermanager.mapper;
 
-import com.sergejs.ordermanager.dto.request.OrderRequest;
 import com.sergejs.ordermanager.dto.response.OrderResponse;
 import com.sergejs.ordermanager.model.Order;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "orderDate", ignore = true)
-    Order map(OrderRequest orderRequest);
 
     OrderResponse map(Order order);
 
--- src/main/java/com/sergejs/ordermanager/service/OrderService.java
+++ src/main/java/com/sergejs/ordermanager/service/OrderService.java
@@ -11,10 +11,31 @@ import java.util.UUID;
 
 public interface OrderService {
 
+    /**
+     * GEt all the orders for the user provided.
+     *
+     * @param userId corresponding user ID.
+     * @return all the orders for that particular user.
+     */
     List<OrderResponse> getAllOrders(UUID userId);
 
+    /**
+     * Get total price within the specific range for specific user.
+     *
+     * @param fromDate lower date bound.
+     * @param toDate   higher date bound.
+     * @param userId   user whose orders have to be taken into account.
+     * @return the total price response.
+     */
     TotalPriceResponse getTotalPriceWithinRange(LocalDate fromDate, LocalDate toDate, UUID userId);
 
+    /**
+     * Save an order for a given user.
+     *
+     * @param userId  user for whom an order has to be created.
+     * @param request order creation request.
+     * @return created oder identifier
+     */
     OrderCreatedResponse save(UUID userId, OrderRequest request);
 
 }
